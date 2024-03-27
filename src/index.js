3e99a8b951320a14adeb2bd774687385219c9082
tanesanni <babichdenis@me.com>
2024-03-27 04:01:30 +0300

    video_screen

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+export default (textRules, generateRoundProperties) => {
+    console.log('Welcome to the Brain Games!');
+
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}`);
+
+    console.log(textRules);
+
+    for (let countRound = 0; countRound < 3; countRound += 1) {
+        const { question, correctAnswer } = generateRoundProperties();
+
+        console.log(`Question: ${question}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+
+        if (correctAnswer !== userAnswer) {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+            console.log(`Let's try again, ${userName}!`);
+            return;
+        }
+        console.log('Correct!');
+    }
+    console.log(`Congratulations, ${userName}!`);
+};
